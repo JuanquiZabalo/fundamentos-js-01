cff7003a719f54d71030f55474c045b77bbce538
JuanquiZabalo <jucazato95@gmail.com>
2023-11-09 20:59:05 +0100

    resolucion ejercicio 3

---
+++ src/ejercicio-03/index.js
@@ -0,0 +1,9 @@
+const numero = 20
+const tipoNumero = numero > 0 ? 'positivo': 'negativo o cero'
+const cadena = 'hola'
+const longitudCadena = cadena.length > 5 ? 'larga': 'corta'
+
+console.log('Numero:', numero)
+console.log('El numero es:', tipoNumero)
+console.log('Cadena:', cadena)
+console.log('Longitud de cadena es:', longitudCadena)
