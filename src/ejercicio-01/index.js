44e2e8fd0f99b1c3a3f0f7e1cea9f9f933b077b3
JuanquiZabalo <jucazato95@gmail.com>
2023-11-09 20:58:32 +0100

    resolucion ejercicio 1

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,12 @@
+const nombre = 'Juan Carlos'
+const edad = 28
+const esEstudiante = true
+const mensaje = 'Hola mi nombre es ' + nombre + ' y tengo ' + edad + ' anios. \
+Soy estudiante ' + esEstudiante
+
+console.log('Nombre: ', nombre)
+console.log('Edad: ', edad)
+console.log('Es estudiante: ', esEstudiante)
+console.log('Mensaje: ', mensaje)
+
+
